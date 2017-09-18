f955ccb5b838328d8f00078382a9babc64f9ebdf
Pedro Zamuner <pzamuner@campeche.chipusnet>
2017-09-18 16:14:27 -0300

    Adding blog post about downloads drop

--- app/back-end/blog/main/urls.py
+++ app/back-end/blog/main/urls.py
@@ -4,5 +4,6 @@ from main import views
                                                                                 
 urlpatterns = [                                                                 
     url(r'^$', views.main, name='main'),
+    url(r'downloads-cairam/$', views.downloads_drop, name='downloads_drop'),
     url(r'^thanks/$', views.thanks, name='thanks'),
 ]
--- app/back-end/blog/main/views.py
+++ app/back-end/blog/main/views.py
@@ -26,3 +26,21 @@ def main(request):
 
 def thanks(request):
     return render(request, 'main/thanks.html')
+
+# Create your views here.
+def downloads_drop(request):
+
+    lead_form = LeadForm(request.POST or None)
+
+    context = {'lead_form': lead_form}
+
+    if lead_form.is_valid():
+        
+        lead = lead_form.save(commit=False)
+        lead.ip = get_client_ip(request)
+        lead.save()
+
+        return redirect(reverse(''))
+
+    return render(request, 'main/downloads_drop.html', context)
+   
