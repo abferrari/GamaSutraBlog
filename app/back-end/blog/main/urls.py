88ca529c3612330be75c9b35a0f72f152a4e98d5
Pedro Zamuner <pedrozamuner@gmail.com>
2017-09-19 11:46:50 -0300

    adding post cinco motivos..

--- app/back-end/blog/main/urls.py
+++ app/back-end/blog/main/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.main, name='main'),
     url(r'downloads-cairam/$', views.downloads_drop, name='downloads_drop'),
     url(r'whatsapp-para-empresas/$', views.whatsapp4business, name='whatsapp4business'),
+    url(r'cinco-motivos-para-apostar-em-um-app/$', views.fivereasons, name='fivereasons'),
     url(r'^thanks/$', views.thanks, name='thanks'),
 ]
