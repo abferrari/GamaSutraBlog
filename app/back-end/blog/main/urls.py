8c7264b982d20bd665d0b0ae0a4d5c3eb80b6121
abferrari <andrea.bonette.ferrari@gmail.com>
2017-09-20 16:31:57 -0300

    Fix url havingapp4business

--- app/back-end/blog/main/urls.py
+++ app/back-end/blog/main/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'whatsapp-para-empresas/$', views.whatsapp4business, name='whatsapp4business'),
     url(r'cinco-motivos-para-apostar-em-um-app/$', views.fivereasons, name='fivereasons'),
     url(r'o-que-considerar-em-um-app-para-o-seu-negocio/$', views.considerations, name='considerations'),
-    url(r'^ter-um-app-para-meu-negocio-e-uma-boa-ideia/$', views.havingapp4business, name='havingapp4business'),
+    url(r'ter-um-app-para-meu-negocio-e-uma-boa-ideia/$', views.havingapp4business, name='havingapp4business'),
     url(r'^thanks/$', views.thanks, name='thanks'),
     url(r'^infografico-uso-smartphone/$', views.lp_info, name='lp_info'),
 
